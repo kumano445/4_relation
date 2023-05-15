550bfefec70ad1bea73aa47ec0e9f30fd6076036
yoshiko <ttkmysk021@gmail.com>
2023-05-15 21:25:01 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1
+var x = 10
+var y = 7
+if (x >= y) {
+  console.log("xはyより大きいまたは等しい");
+}
+
+// 2
+var x = 7
+var y = 10
+if (x <= y){
+  console.log("xがyより小さいまたは等しい");
+}
+
+// 3
+var x = 10
+var y = 7
+if (x > y) {
+  console.log("xがy以上");
+}
+
+// 4
+
+var x = 7
+var y = 10
+if (x < y){
+  console.log("xがy以下");
+}
